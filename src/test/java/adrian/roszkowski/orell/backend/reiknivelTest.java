4ec0e7c8cb7b32b35824adc4a57742a3b8078136
Ji Xiang <xij1@hi.is>
2023-04-20 18:54:58 +0000

    css

---
+++ src/test/java/adrian/roszkowski/orell/backend/reiknivelTest.java
@@ -0,0 +1,30 @@
+package adrian.roszkowski.orell.backend;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class reiknivelTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void setFunction() {
+    }
+
+    @Test
+    void setValue() {
+    }
+
+    @Test
+    void calculate() {
+    }
+
+    @Test
+    void reset() {
+    }
+}
---
+++ src/test/java/adrian/roszkowski/orell/frontend/reiknivelApplicationTest.java
@@ -0,0 +1,23 @@
+package adrian.roszkowski.orell.frontend;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class reiknivelApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void start() {
+
+    }
+
+    @Test
+    void main() {
+    }
+}
