ea8b75f58c2b6e4a0106125940fc17d4ed390ba2
Ji Xiang <xij1@hi.is>
2023-04-21 19:41:30 +0000

    css

--- src/test/java/adrian/roszkowski/orell/backend/reiknivelTest.java
+++ src/test/java/adrian/roszkowski/orell/backend/reiknivelTest.java
@@ -15,29 +15,78 @@ class reiknivelTest {
     @Test
     void calculate() {
         //TODO make a test for this function
-       Calculate addition = (int a, int b) -> {
+        Calculate calculator = new Calculate() {
+            @Override
+            public int calculate(int a, int b) {
+                return a + b;
+            }
+        };
+        double result = calculator.calculate(2, 3);
+        assertEquals(5.0, result, 0.0001);
 
-           return a + b;
-       };
+            Calculate calculator1 = new Calculate() {
+                @Override
+                public int calculate(int a, int b) {
+                    return a-b;
+                }
+            };
+            double result2 = calculator.calculate(5,  3);
+            assertEquals(2.0, result, 0.0001);
+
+            Calculate calculator3 = new Calculate() {
+                @Override
+                public int calculate(int a, int b) {
+                    return a*b;
+                }
+            };
+            double result3 = calculator.calculate(2,  3);
+            assertEquals(6, result, 0.0001);
+
+            Calculate calculate4 = new Calculate() {
+                @Override
+                public int calculate(int a, int b) {
+                    return a/b;
+                }
+            };
+            double result4 = calculator.calculate(10,  2);
+            assertEquals(5, result, 0.0001);
 
 
-        Calculate decrement = (int a, int b) -> {
-           return a - b;
-       };
-       Calculate division = (int a, int b) -> {
-           return a / b;
-       };
-       Calculate multiplication = (int a, int b) -> {
-           return a * b;
-       };
 
 
-       }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
     @Test
     void reset() {
+
     }
 }
